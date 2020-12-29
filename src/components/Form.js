a0b512549cbd288cd4878d9aa96082a1f77a364a
Nathaniel Chang <namachan@ucsc.edu>
2020-12-28 21:45:26 -0800

    Started to do list react project. Created starting state and hooks.

---
+++ src/components/Form.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Form = ( { setInputText, todos, setTodos, inputText } ) => {
+    // Here I can write Javascript code and functions
+    // Reads input and prints to the console
+    const inputTextHandler = ( e ) => {
+        console.log(e.target.value);
+        setInputText(e.target.value);
+    };
+
+    // In the future, install a package to get a unique idea. This way is stupid.
+    const submitTodoHandler = (e) => {
+        e.preventDefault();
+        setTodos([
+            ...todos, { text: inputText, completed: false, id: Math.random() * 1000 }
+        ]);
+        setInputText("");
+    }
+    return(
+        <form>
+        <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
+        <button onClick={submitTodoHandler} className="todo-button" type="submit">
+            <i className="fas fa-plus-square"></i>
+        </button>
+        <div className="select">
+            <select name="todos" className="filter-todo">
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="uncompleted">Uncompleted</option>
+            </select>
+        </div>
+        </form>
+    );
+};
+
+export default Form;
